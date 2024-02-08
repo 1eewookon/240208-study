3938e9d38edbecab8659101dc03ee45a70e29b7d
wookon <wookon@naver.com>
2024-02-08 12:01:10 +0900

    [2024-02-08 수업 내용]

--- src/main/java/com/ohgiraffers/section01/conditional/Application01.java
+++ src/main/java/com/ohgiraffers/section01/conditional/Application01.java
@@ -5,7 +5,15 @@ public class Application01 {
     public static void main(String[] args) {
 
         A_if a = new A_if();
-        a.testSimpleIfStatement();
+//        a.testSimpleIfStatement();
+//        a.testNestedIfStatement();
+
+        B_ifElse b = new B_ifElse();
+//        b.testSimpleIfElseStatement();
+//        b.testNestedIfElseStatement();
+
+        C_ifElseIf c = new C_ifElseIf();
+        c.testSimpleIfElseIfStatement();
 
     }
 
