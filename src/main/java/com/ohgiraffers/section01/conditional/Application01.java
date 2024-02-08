dd136bf3f24886ac6b9fb5c2d3cbe7d0ac02ce32
wookon <wookon@naver.com>
2024-02-08 10:10:27 +0900

    [2024-02-08 수업 내용]

---
+++ src/main/java/com/ohgiraffers/section01/conditional/Application01.java
@@ -0,0 +1,12 @@
+package com.ohgiraffers.section01.conditional;
+
+public class Application01 {
+
+    public static void main(String[] args) {
+
+        A_if a = new A_if();
+        a.testSimpleIfStatement();
+
+    }
+
+}
